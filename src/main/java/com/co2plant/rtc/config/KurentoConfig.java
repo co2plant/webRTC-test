89997ad9212d62c5abf14716220715f52c6c56f2
co2plant <jaonz6057@wku.ac.kr>
2026-01-29 11:38:42 +0900

    fix : kurento server config update

--- src/main/java/com/co2plant/rtc/config/KurentoConfig.java
+++ src/main/java/com/co2plant/rtc/config/KurentoConfig.java
@@ -4,11 +4,16 @@ import org.kurento.client.KurentoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Configuration
 public class KurentoConfig {
 
+    @Value("${kurento.client.url}")
+    private String kurentoRawUrl;
+
     @Bean
     public KurentoClient kurentoClient() {
-        return KurentoClient.create();
+        return KurentoClient.create(kurentoRawUrl);
     }
 }
